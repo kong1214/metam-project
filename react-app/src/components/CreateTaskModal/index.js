6f539e3406f95efe6e3bd32f5c5bb944cf2da2b0
kong1214 <kevin.ong@Live.com>
2023-02-24 22:49:10 -0800

    need to finish adding task

--- react-app/src/components/CreateTaskModal/index.js
+++ react-app/src/components/CreateTaskModal/index.js
@@ -15,6 +15,7 @@ function CreateTaskFormModal({}) {
     const [taskName, setTaskName] = useState("");
     const [priority, setPriority] = useState("");
     const [taskStatus, setTaskStatus] = useState("");
+    const [projectSection, setProjectSection] = useState("")
     const [description, setDescription] = useState("");
     const [dueDate, setDueDate] = useState("");
     const [errors, setErrors] = useState([]);
@@ -37,6 +38,16 @@ function CreateTaskFormModal({}) {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        const newTask = {
+            task_name: taskName,
+            priority: priority,
+            task_status: taskStatus,
+            project_section: projectSection,
+            description: description,
+            due_date: dateParser(dueDate),
+            created_at: date,
+            updated_at: date
+        }
 
     };
 
@@ -83,12 +94,33 @@ function CreateTaskFormModal({}) {
                     <option value="At Risk">At Risk</option>
                     <option value="Off Track">Off Track</option>
                 </select>
+                <label for="task-project-section-dropdown">Project Section</label>
+                <select
+                    id="task-project-section-dropdown"
+                    className="dropdown-create"
+                    value={projectSection}
+                    onChange={(e) => setProjectSection(e.target.value)}
+                    required
+                >
+                    <option value="">Select a section</option>
+                    <option value="To do">To do</option>
+                    <option value="Doing">Doing</option>
+                    <option value="Done">Done</option>
+                </select>
                 <label for="dueDate">Due Date</label>
                 <input
                     type="date"
                     id="dueDate"
                     value={dueDate}
                     onChange={(e) => setDueDate(e.target.value)}
+                    required
+                />
+                <label for="task-description-input">Task Description</label>
+                <textarea
+                    id="task-description-input"
+                    type="textarea"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
                     required
                 />
                 <button type="submit">Create</button>
