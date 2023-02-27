9c5a2418f80e07a7f85e3a66fce762179d9bc8e1
kong1214 <kevin.ong@Live.com>
2023-02-26 23:55:34 -0800

    finished delete modal styling

--- react-app/src/components/DeleteTaskModal/index.js
+++ react-app/src/components/DeleteTaskModal/index.js
@@ -4,8 +4,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { deleteTask } from "../../store/task";
 import { Redirect, useHistory, useParams } from "react-router-dom";
 import { useModal } from "../../context/Modal";
+import "./DeleteTaskModal.css"
 
-function DeleteTaskModal({taskId}) {
+function DeleteTaskModal({ taskId }) {
   const dispatch = useDispatch();
   const history = useHistory()
   const { closeModal } = useModal();
@@ -13,15 +14,17 @@ function DeleteTaskModal({taskId}) {
   const handleDelete = async () => {
 
     return dispatch(deleteTask(taskId))
-    .then(() => closeModal())
+      .then(() => closeModal())
   }
 
   return (
-    <>
-      <h1>Delete the task?</h1>
-      <button onClick={() => closeModal()}>Cancel</button>
-      <button onClick={() => handleDelete()}>Delete</button>
-    </>
+    <div className="delete-modal-container">
+      <div className="delete-modal-header">Delete the task?</div>
+      <div className="delete-modal-buttons">
+        <button className="delete-modal-cancel-button" onClick={() => closeModal()}>Cancel</button>
+        <button className="delete-modal-delete-button" onClick={() => handleDelete()}>Delete</button>
+      </div>
+    </div>
   );
 }
 
--- react-app/src/components/EditTaskModal/index.js
+++ react-app/src/components/EditTaskModal/index.js
@@ -56,80 +56,96 @@ function EditTaskFormModal({task}) {
     };
 
     return (
-        <>
-            <h1>Edit Task</h1>
-            <form onSubmit={handleSubmit}>
+        <div id="create-task-container">
+            <div className="create-task-header">Edit Task</div>
+            <form id="create-task-form-container" onSubmit={handleSubmit}>
                 <ul>
                     {errors.map((error, idx) => (
                         <li key={idx}>{error}</li>
                     ))}
                 </ul>
-                <label for="task-name-input">Task Name</label>
-                <input
-                    id="task-name-input"
-                    type="text"
-                    value={taskName}
-                    onChange={(e) => setTaskName(e.target.value)}
-                    required
-                />
-                <label for="task-priority-dropdown">Task Priority</label>
-                <select
-                    id="task-priority-dropdown"
-                    className="dropdown-create"
-                    value={priority}
-                    onChange={(e) => setPriority(e.target.value)}
-                    required
-                >
-                    <option value="">Select a priority</option>
-                    <option value="Low">Low</option>
-                    <option value="Medium">Medium</option>
-                    <option value="High">High</option>
-                </select>
-                <label for="task-status-dropdown">Task Status</label>
-                <select
-                    id="task-status-dropdown"
-                    className="dropdown-create"
-                    value={taskStatus}
-                    onChange={(e) => setTaskStatus(e.target.value)}
-                    required
-                >
-                    <option value="">Select a status</option>
-                    <option value="On Track">On Track</option>
-                    <option value="At Risk">At Risk</option>
-                    <option value="Off Track">Off Track</option>
-                </select>
-                <label for="task-project-section-dropdown">Project Section</label>
-                <select
-                    id="task-project-section-dropdown"
-                    className="dropdown-create"
-                    value={projectSection}
-                    onChange={(e) => setProjectSection(e.target.value)}
-                    required
-                >
-                    <option value="">Select a section</option>
-                    <option value="To do">To do</option>
-                    <option value="Doing">Doing</option>
-                    <option value="Done">Done</option>
-                </select>
-                <label for="dueDate">Due Date</label>
-                <input
-                    type="date"
-                    id="dueDate"
-                    value={dueDate}
-                    onChange={(e) => setDueDate(e.target.value)}
-                    required
-                />
-                <label for="task-description-input">Task Description</label>
-                <textarea
-                    id="task-description-input"
-                    type="textarea"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    required
-                />
-                <button type="submit">Edit</button>
+                <div id="create-project-form-name-container" className="label-input-container">
+                    <label id="task-name-input-label">Task Name</label>
+                    <input
+                        id="task-name-input"
+                        type="text"
+                        value={taskName}
+                        onChange={(e) => setTaskName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div id="create-task-priority-status-container">
+                    <div id="task-priority-container" className="label-input-container">
+                        <label id="task-priority-dropdown-label">Task Priority</label>
+                        <select
+                            id="task-priority-dropdown-input"
+                            className="dropdown-create"
+                            value={priority}
+                            onChange={(e) => setPriority(e.target.value)}
+                            required
+                        >
+                            <option value="">Select a priority</option>
+                            <option value="Low">Low</option>
+                            <option value="Medium">Medium</option>
+                            <option value="High">High</option>
+                        </select>
+                    </div>
+                    <div id="task-status-container" className="label-input-container">
+                        <label id="task-status-dropdown-label">Task Status</label>
+                        <select
+                            id="task-status-dropdown-input"
+                            className="dropdown-create"
+                            value={taskStatus}
+                            onChange={(e) => setTaskStatus(e.target.value)}
+                            required
+                        >
+                            <option value="">Select a status</option>
+                            <option value="On Track">On Track</option>
+                            <option value="At Risk">At Risk</option>
+                            <option value="Off Track">Off Track</option>
+                        </select>
+                    </div>
+                </div>
+                <div id="create-task-section-dueDate-container">
+                    <div id="create-task-section-container" className="label-input-container">
+                        <label id="task-project-section-dropdown-label">Project Section</label>
+                        <select
+                            id="task-project-section-dropdown-input"
+                            className="dropdown-create"
+                            value={projectSection}
+                            onChange={(e) => setProjectSection(e.target.value)}
+                            required
+                        >
+                            <option value="">Select a section</option>
+                            <option value="To do">To do</option>
+                            <option value="Doing">Doing</option>
+                            <option value="Done">Done</option>
+                        </select>
+                    </div>
+                    <div id="create-task-dueDate-container" className="label-input-container">
+                        <label id="create-task-dueDate-label">Due Date</label>
+                        <input
+                            type="date"
+                            id="create-task-dueDate-input"
+                            value={dueDate}
+                            onChange={(e) => setDueDate(e.target.value)}
+                            required
+                        />
+                    </div>
+                </div>
+                <div id="task-description-container">
+                    <label id="task-description-input-label">Task Description</label>
+                    <textarea
+                        id="task-description-input"
+                        type="textarea"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required
+                    />
+                </div>
+                <button id="create-task-button" type="submit">Edit</button>
             </form>
-        </>
+        </div>
     );
 }
 
