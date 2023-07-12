d611a2c1abfb618de9e256c90e703d839b1b9b73
kong1214 <kevin.ong@Live.com>
2023-07-12 13:42:14 -0700

    added themes to change navbar sidebar and main content

---
+++ react-app/src/constants/siteThemes.js
@@ -0,0 +1,27 @@
+export const siteThemes = {
+  default: {
+    primary: "#9CA1AE", // main ontent
+    secondary: "#363639", // navbar
+    tertiary: "#2e2e30", // sidebar
+  },
+  natural: {
+    primary: "#E8EDE6",
+    secondary: "#253825",
+    tertiary: "#5B715B",
+  },
+  lavendar: {
+    primary: "#EDE8FF",
+    secondary: "#8466F2",
+    tertiary: "#A48DF7",
+  },
+  urban: {
+    primary: "#F0F2F1",
+    secondary: "#21BFA2",
+    tertiary: "#4E5D67",
+  },
+  ember: {
+    primary: "#F2EEE9",
+    secondary: "#F26A4B",
+    tertiary: "#8C5649",
+  },
+};
