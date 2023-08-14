313c71706cfd0fb739dde99a71c94452f9c8dfc7
kong1214 <kevin.ong@Live.com>
2023-08-13 18:46:07 -0700

    fixed assignee

--- react-app/src/components/TaskList/TaskAssignee.js
+++ react-app/src/components/TaskList/TaskAssignee.js
@@ -17,6 +17,7 @@ function TaskAssignee({ task, assignee, users }) {
     useEffect(() => {
         if (!showMenu) return;
 
+        console.log(showMenu)
         const closeMenu = (e) => {
             if (!ulRef.current.contains(e.target)) {
                 setShowMenu(false);
@@ -33,6 +34,7 @@ function TaskAssignee({ task, assignee, users }) {
     let assigneeOutput
     typeof assignee === "string" ? assigneeOutput = assignee : assigneeOutput = `${assignee.first_name} ${assignee.last_name}`
     const ulClassName = "task-assignee-dropdown" + (showMenu ? "" : " hidden");
+    console.log(ulClassName)
     const closeMenu = () => setShowMenu(false);
 
     const handleEdit = async (taskId, newAssigneeId) => {
