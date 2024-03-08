d473831703c20208a15630df53d6410fd0b7920d
kong1214 <kevin.ong@Live.com>
2024-03-07 21:02:53 -0800

    enhancement: incorporated new modal into delete project

--- react-app/src/components/Modals/DeleteModal.js
+++ react-app/src/components/Modals/DeleteModal.js
@@ -3,45 +3,53 @@ import { useDispatch } from "react-redux";
 import { deleteSection } from "../../store/section"
 import { deleteTask } from "../../store/task"
 import { deleteProject } from "../../store/project"
-import { useModal } from "../../../context/Modal";
+import { useModal } from "../../context/Modal";
 
 function DeleteModal({ type, item }) {
-  const dispatch = useDispatch();
-  const { closeModal } = useModal();
+    const dispatch = useDispatch();
+    const { closeModal } = useModal();
 
-  const handleDelete = async () => {
+    const handleDelete = async () => {
 
-    switch (type) {
-        case 'project':
-            return dispatch(deleteProject(item.id))
-            .then(() => closeModal())
-            break;
-        case 'section':
-            return dispatch(deleteSection(item.id))
-            .then(() => closeModal())
-            break;
-        case 'task':
-            return dispatch(deleteTask(item.id))
-            .then(() => closeModal())
-            break;
-        default:
-            break;
+        switch (type) {
+            case 'project':
+                return dispatch(deleteProject(item.id))
+                    .then(() => closeModal())
+                break;
+            case 'section':
+                return dispatch(deleteSection(item.id))
+                    .then(() => closeModal())
+                break;
+            case 'task':
+                return dispatch(deleteTask(item.id))
+                    .then(() => closeModal())
+                break;
+            default:
+                break;
+        }
     }
-  }
 
-  return (
-    <div className="delete-modal-container">
-      <div className="delete-modal-header">Delete Section: {item.name}?</div>
-      <div className="delete-section-content-container">
-        <div id="delete-section-warning">This will delete the project, along with any:</div>
-        <li className="each-deletion">Tasks that are in this section</li>
-      </div>
-      <div className="delete-modal-buttons">
-        <button className="delete-modal-cancel-button" onClick={() => closeModal()}>Cancel</button>
-        <button className="delete-modal-delete-button" onClick={() => handleDelete()}>Delete</button>
-      </div>
-    </div>
-  );
+    let modalBullets = []
+    if (type === 'project') {
+        modalBullets = ['Sections that are in this project', 'Tasks that are in this project']
+    } else if (type === 'section') {
+        modalBullets = ['Tasks that are in this project']
+    }
+    return (
+        <div className="delete-modal-container">
+            <div className="delete-modal-header">{`Delete ${item.name}?`}</div>
+            {type !== 'task' && <div className="delete-section-content-container">
+                <div id="delete-section-warning">This will delete the {type}, along with any:</div>
+                {modalBullets.map((bullet, index) => (
+                    <div key={index}>{bullet}</div>
+                ))}
+            </div>}
+            <div className="delete-modal-buttons">
+                <button className="delete-modal-cancel-button" onClick={() => closeModal()}>Cancel</button>
+                <button className="delete-modal-delete-button" onClick={() => handleDelete()}>Delete</button>
+            </div>
+        </div>
+    );
 }
 
 export default DeleteModal;
